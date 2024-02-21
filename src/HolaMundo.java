5aac6307679c381e449d2c7a4f3006c7e83f0c1c
MNIZE <martin.ansia@iessanmamede.com>
2024-02-21 11:36:01 +0100

    Se agrego la clase HolaMundo

---
+++ src/HolaMundo.java
@@ -0,0 +1,7 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        Mensaje msg = new Mensaje();
+        String nombre = msg.solicitaDatos("Escribe tu nombre");
+        msg.saludar(nombre);
+    }
+}
